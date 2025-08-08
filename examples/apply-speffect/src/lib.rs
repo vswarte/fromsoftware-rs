0d6f4ffe20723921462ec2ef516e6ef9b815cfc8
Vincent Swarte <vincent@vinsert.nl>
2025-08-09 01:28:51 +0200

    Satisfy clippy

--- examples/apply-speffect/src/lib.rs
+++ examples/apply-speffect/src/lib.rs
@@ -14,7 +14,7 @@ const SP_EFFECT: i32 = 4330;
 /// # Safety
 /// This is exposed this way such that libraryloader can call it. Do not call this yourself.
 #[no_mangle]
-pub unsafe extern "C" fn DllMain(hmodule: u64, reason: u32) -> bool {
+pub unsafe extern "C" fn DllMain(_hmodule: u64, reason: u32) -> bool {
     // Exit early if we're not attaching a DLL
     if reason == 1 {
         return true;
