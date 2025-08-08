63d35a6a7f48a4a73c1571eac9f9e04550ae5505
Vincent Swarte <vincent@vinsert.nl>
2025-08-09 01:25:28 +0200

    Cleanup speffect example a bit

--- examples/apply-speffect/src/lib.rs
+++ examples/apply-speffect/src/lib.rs
@@ -15,39 +15,44 @@ const SP_EFFECT: i32 = 4330;
 /// This is exposed this way such that libraryloader can call it. Do not call this yourself.
 #[no_mangle]
 pub unsafe extern "C" fn DllMain(hmodule: u64, reason: u32) -> bool {
-    // Check if DLL is being attached
+    // Exit early if we're not attaching a DLL
     if reason == 1 {
-        std::thread::spawn(move || {
-            wait_for_system_init(&Program::current(), Duration::MAX)
-                .expect("Timeout waiting for system init");
-
-            // Retrieve games task runner.
-            let cs_task = get_instance::<CSTaskImp>().unwrap().unwrap();
-            cs_task.run_recurring(
-                // The registered task will be our closure.
-                |_: &FD4TaskData| {
-                    let Some(world_chr_man) = unsafe { get_instance::<WorldChrMan>() }.unwrap()
-                    else {
-                        return;
-                    };
-
-                    let Some(ref mut main_player) = world_chr_man.main_player else {
-                        return;
-                    };
-
-                    if input::is_key_pressed(0x4F) {
-                        main_player.chr_ins.apply_speffect(SP_EFFECT, true);
-                    }
-
-                    if input::is_key_pressed(0x50) {
-                        main_player.chr_ins.remove_speffect(SP_EFFECT);
-                    }
-
-                },
-                CSTaskGroupIndex::FrameBegin,
-            );
-        });
+        return true;
     }
+
+    std::thread::spawn(move || {
+        wait_for_system_init(&Program::current(), Duration::MAX)
+            .expect("Timeout waiting for system init");
+
+        // Retrieve games task runner and register a task at frame begin.
+        let cs_task = get_instance::<CSTaskImp>().unwrap().unwrap();
+        cs_task.run_recurring(
+            |_: &FD4TaskData| {
+                // Retrieve WorldChrMan
+                let Some(world_chr_man) = unsafe { get_instance::<WorldChrMan>() }.unwrap()
+                else {
+                    return;
+                };
+
+                // Retrieve main player
+                let Some(ref mut main_player) = world_chr_man.main_player else {
+                    return;
+                };
+
+                // Check if "o" is pressed
+                if input::is_key_pressed(0x4F) {
+                    main_player.chr_ins.apply_speffect(SP_EFFECT, true);
+                }
+
+                // Check if "p" is pressed
+                if input::is_key_pressed(0x50) {
+                    main_player.chr_ins.remove_speffect(SP_EFFECT);
+                }
+
+            },
+            CSTaskGroupIndex::FrameBegin,
+        );
+    });
 
     true
 }
