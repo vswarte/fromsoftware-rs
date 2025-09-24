6c098dcd3b8454f00d4f85840f180d8b8cbc805a
Vincent Swarte <vincent@vinsert.nl>
2025-09-24 11:17:02 +0200

    Correct publish script to include new macros crate

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cargo publish --allow-dirty --no-verify -p fromsoftware-dlrf-derive
-cargo publish --allow-dirty --no-verify -p fromsoftware-dlrf
 cargo publish --allow-dirty --no-verify -p fromsoftware-shared
+cargo publish --allow-dirty --no-verify -p fromsoftware-shared-macros
 cargo publish --allow-dirty --no-verify -p eldenring
 cargo publish --allow-dirty --no-verify -p eldenring-util
