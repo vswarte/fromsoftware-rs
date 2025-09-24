18d816f43ce9c951460a9359ed3d0d86789212d3
Natalie Weizenbaum <nex342@gmail.com>
2025-09-23 21:27:11 -0700

    [DS3] Add task management

---
+++ crates/darksouls3/src/fd4/basic_hash_string.rs
@@ -0,0 +1,28 @@
+use std::{ffi, fmt::Display};
+
+use crate::dltx::{DLString, DLStringKind, DLUTF16StringKind};
+
+#[repr(C)]
+/// Wraps a string to make it easier to use with hashmaps. Seemingly mostly used in the resource
+/// system but has some usage elsewhere too.
+///
+/// Source of name: RTTI
+pub struct FD4BasicHashString<T: DLStringKind = DLUTF16StringKind> {
+    vftable: usize,
+    /// The contained string we're hashing for.
+    pub inner: DLString<T>,
+    // The rest of this is probably the same as in ER, but this hasn't been
+    // verified yet.
+}
+
+impl<T: DLStringKind> AsRef<DLString<T>> for FD4BasicHashString<T> {
+    fn as_ref(&self) -> &DLString<T> {
+        &self.inner
+    }
+}
+
+impl<T: DLStringKind> Display for FD4BasicHashString<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.inner.fmt(f)
+    }
+}
