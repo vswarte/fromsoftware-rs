6bb6c616fd61363aab79045db5cce0a3df9e69f5
Natalie Weizenbaum <nex342@gmail.com>
2025-09-23 21:27:11 -0700

    [DS3] Add task management

---
+++ crates/darksouls3/src/dlkr.rs
@@ -0,0 +1,28 @@
+use std::ptr::NonNull;
+
+use vtable_rs::VPtr;
+
+#[vtable_rs::vtable]
+pub trait DLAllocatorVmt {
+    fn destructor(&mut self, param_2: bool);
+}
+
+pub struct DLAllocatorBase {
+    pub vftable: VPtr<dyn DLAllocatorVmt, Self>,
+}
+
+#[repr(transparent)]
+#[derive(Clone)]
+pub struct DLAllocatorRef(NonNull<DLAllocatorBase>);
+
+impl From<NonNull<DLAllocatorBase>> for DLAllocatorRef {
+    fn from(ptr: NonNull<DLAllocatorBase>) -> Self {
+        Self(ptr)
+    }
+}
+
+impl DLAllocatorVmt for DLAllocatorBase {
+    extern "C" fn destructor(&mut self, param_2: bool) {
+        todo!()
+    }
+}
---
+++ crates/darksouls3/src/rva/rva_data.rs
@@ -0,0 +1,1 @@
+pub const RVA_REGISTER_TASK: u32 = 0xf0fa50;
