aa556133510c11c157f7cd4fdc8a928b09dae358
Vincent Swarte <vincent@vinsert.nl>
2025-10-19 06:00:05 +0200

    Rename sync parameter to dont_sync in apply_speffect

--- crates/eldenring-util/src/chr_ins.rs
+++ crates/eldenring-util/src/chr_ins.rs
@@ -7,13 +7,13 @@ use crate::rva;
 use shared::program::Program;
 
 pub trait ChrInsExt {
-    fn apply_speffect(&mut self, sp_effect: i32, sync: bool);
+    fn apply_speffect(&mut self, sp_effect: i32, dont_sync: bool);
 
     fn remove_speffect(&mut self, sp_effect: i32);
 }
 
 impl ChrInsExt for ChrIns {
-    fn apply_speffect(&mut self, sp_effect: i32, sync: bool) {
+    fn apply_speffect(&mut self, sp_effect: i32, dont_sync: bool) {
         let rva = Program::current()
             .rva_to_va(rva::get().chr_ins_apply_speffect)
             .unwrap();
