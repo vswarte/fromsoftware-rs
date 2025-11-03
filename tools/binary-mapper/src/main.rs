a01ac484d7249280801b28a4147900e039a01cab
Natalie Weizenbaum <nex342@gmail.com>
2025-11-03 11:22:59 -0800

    Code review

--- tools/binary-mapper/src/main.rs
+++ tools/binary-mapper/src/main.rs
@@ -8,7 +8,7 @@ use pelite::{
 };
 use rayon::prelude::*;
 use serde::Deserialize;
-use shared::find_rtti_classes;
+use shared::{find_rtti_classes, Class};
 
 #[derive(ValueEnum, Clone)]
 enum OutputFormat {
@@ -45,59 +45,15 @@ fn main() {
     let profile: MapperProfile = toml::from_str(&contents).expect("Could not parse profile TOML");
 
     let result = profile
-        .offsets
+        .patterns
         .into_par_iter()
-        .flat_map(|entry| {
-            match entry {
-                MapperOffset::Pattern { pattern, captures } => {
-                    let Ok(scanner_pattern) = pattern::parse(&pattern) else {
-                        panic!("Could not parse provided pattern \"{}\"", &pattern)
-                    };
-
-                    let mut matches = vec![0u32; captures.len()];
-                    let captures = captures.iter().enumerate().filter(|(_, e)| !e.is_empty());
-
-                    if !program
-                        .scanner()
-                        .matches_code(&scanner_pattern)
-                        .next(&mut matches)
-                    {
-                        captures
-                            .map(|(_, e)| MapperEntryResult::not_found(e))
-                            .collect::<Vec<_>>()
-                    } else {
-                        captures
-                            .map(|(i, e)| MapperEntryResult {
-                                name: e.clone(),
-                                rva: matches[i],
-                            })
-                            .collect::<Vec<_>>()
-                    }
-                }
-                MapperOffset::Rtti { class, captures } => {
-                    let Some(class) = rtti_map.get(class.as_str()) else {
-                        panic!("No RTTI class named {}", class);
-                    };
-
-                    captures
-                        .into_iter()
-                        .map(|(name, index)| {
-                            // Safety: We're not actually dereferencing the VA.
-                            if let Some(va) = unsafe { class.vmt_fn(index) } {
-                                if let Ok(rva) = program.va_to_rva(va) {
-                                    return MapperEntryResult {
-                                        name: name.clone(),
-                                        rva,
-                                    };
-                                }
-                            }
-
-                            MapperEntryResult::not_found(name)
-                        })
-                        .collect::<Vec<_>>()
-                }
-            }
-        })
+        .flat_map(|entry| entry.find(&program))
+        .chain(
+            profile
+                .vmts
+                .into_par_iter()
+                .flat_map(|entry| entry.find(&program, &rtti_map)),
+        )
         .collect::<Vec<_>>();
 
     match args.output {
@@ -115,35 +71,95 @@ fn main() {
 /// Profile describing what offsets to extract from a game binary.
 #[derive(Debug, Deserialize)]
 struct MapperProfile {
-    pub offsets: Vec<MapperOffset>,
+    pub patterns: Vec<MapperProfilePattern>,
+    pub vmts: Vec<MapperProfileVmt>,
 }
 
-/// A single offset (or multiple in the case of a pattern) to extract from a
-/// game binary.
+/// A Pelite pattern which matches one or more offsets.
 #[derive(Debug, Deserialize)]
-#[serde(untagged)]
-enum MapperOffset {
-    /// A Pelite pattern which matches one or more offsets.
-    Pattern {
-        /// Pattern used for matching. Under the hood this uses pelite's parser.
-        /// As such, the same pattern syntax is used. More:
-        /// https://docs.rs/pelite/latest/pelite/pattern/fn.parse.html
-        pattern: String,
+struct MapperProfilePattern {
+    /// Pattern used for matching. Under the hood this uses pelite's parser.
+    /// As such, the same pattern syntax is used. More:
+    /// https://docs.rs/pelite/latest/pelite/pattern/fn.parse.html
+    pattern: String,
 
-        /// Names for the captures. These names can be referenced from the
-        /// generated definition file.
-        captures: Vec<String>,
-    },
+    /// Names for the captures. These names can be referenced from the
+    /// generated definition file.
+    captures: Vec<String>,
+}
 
-    /// An RTTI class that provides access to its virtual method table.
-    Rtti {
-        /// The class name, according to the RTTI data in the executable.
-        class: String,
+impl MapperProfilePattern {
+    /// Consumes self and looks up the pattern in [program].
+    fn find<'a>(self, program: &impl Pe<'a>) -> Vec<MapperEntryResult> {
+        let Ok(scanner_pattern) = pattern::parse(&self.pattern) else {
+            panic!("Could not parse provided pattern \"{}\"", &self.pattern)
+        };
 
-        /// A map from names for the captures to indexes in the VMT whose
-        /// values are be used as VMTs.
-        captures: HashMap<String, u32>,
-    },
+        let mut matches = vec![0u32; self.captures.len()];
+        let captures = self
+            .captures
+            .into_iter()
+            .enumerate()
+            .filter(|(_, e)| !e.is_empty());
+
+        if !program
+            .scanner()
+            .matches_code(&scanner_pattern)
+            .next(&mut matches)
+        {
+            captures
+                .map(|(_, e)| MapperEntryResult::not_found(e))
+                .collect::<Vec<_>>()
+        } else {
+            captures
+                .map(|(i, e)| MapperEntryResult {
+                    name: e,
+                    rva: matches[i],
+                })
+                .collect::<Vec<_>>()
+        }
+    }
+}
+
+/// An RTTI class that provides access to its virtual method table.
+#[derive(Debug, Deserialize)]
+struct MapperProfileVmt {
+    /// The class name, according to the RTTI data in the executable.
+    class: String,
+
+    /// A map from names for the captures to indexes in the VMT whose values are
+    /// be used as VMTs.
+    captures: HashMap<String, u32>,
+}
+
+impl MapperProfileVmt {
+    /// Consumes self and looks up the VMT in [rtti_map].
+    fn find<'a, T: Pe<'a>>(
+        self,
+        program: &T,
+        rtti_map: &HashMap<String, Class<'a, T>>,
+    ) -> Vec<MapperEntryResult> {
+        let Some(class) = rtti_map.get(self.class.as_str()) else {
+            panic!("No RTTI class named {}", self.class);
+        };
+
+        self.captures
+            .into_iter()
+            .map(|(name, index)| {
+                // Safety: We're not actually dereferencing the VA.
+                if let Some(va) = unsafe { class.vmt_fn(index) } {
+                    if let Ok(rva) = program.va_to_rva(va) {
+                        return MapperEntryResult {
+                            name: name,
+                            rva,
+                        };
+                    }
+                }
+
+                MapperEntryResult::not_found(name)
+            })
+            .collect::<Vec<_>>()
+    }
 }
 
 /// Result of one of the entry items.
