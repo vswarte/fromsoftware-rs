bca6b18be75d1916c6b52bf9476e8bc09cdc8b3a
Vincent Swarte <vincent@vinsert.nl>
2025-11-12 00:52:52 +0100

    Reexport from-singleton to prevent people from having to import from-singleton themselves

--- crates/shared/macros/src/lib.rs
+++ crates/shared/macros/src/lib.rs
@@ -18,7 +18,7 @@ pub fn singleton(args: TokenStream, input: TokenStream) -> TokenStream {
     TokenStream::from(quote! {
         #input_struct
 
-        impl ::from_singleton::FromSingleton for #input_struct_ident {
+        impl ::shared::FromSingleton for #input_struct_ident {
             fn name() -> ::std::borrow::Cow<'static, str> {
                 ::std::borrow::Cow::Borrowed(#dlrf_name)
             }
