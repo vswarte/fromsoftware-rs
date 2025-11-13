102b25bce5c387eaaf296ed5c037a16a43a0945c
Vincent Swarte <vincent@vinsert.nl>
2025-11-13 22:59:23 +0100

    Correct unk offset

--- crates/eldenring/src/cs/chr_ins.rs
+++ crates/eldenring/src/cs/chr_ins.rs
@@ -1788,7 +1788,7 @@ pub enum ChrType {
 pub struct CSChrFallModule {
     vftable: usize,
     pub owner: NonNull<ChrIns>,
-    unk8: i64,
+    unk10: i64,
     pub fall_timer: f32,
     hamari_fall_death_checked: bool,
     pub force_max_fall_height: bool,
