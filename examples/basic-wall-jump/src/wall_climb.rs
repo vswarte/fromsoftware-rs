2a45c01eb6030f5455d9cb1cf1a36f9803f5dd13
lndura <146878622+lndura@users.noreply.github.com>
2025-12-16 22:35:50 +0100

    Add documentation

--- examples/basic-wall-jump/src/wall_climb.rs
+++ examples/basic-wall-jump/src/wall_climb.rs
@@ -4,8 +4,13 @@ pub const JUMP_TIME_FRAME: Duration = Duration::from_millis(350);
 
 #[repr(C)]
 pub struct WallJumpManager {
+    /// Tracks the time for the jump window.
+    /// This is updated when "has_entered_window" is set to true.
     pub window_enter_time: Instant,
+    /// Tracks if we entered the time window to jump.
     pub has_entered_window: bool,
+    /// Represents wether a wall-jump was activated
+    /// You cannot do another untill the `is_jumping` field in the ChrIns became false.
     pub has_jumped: bool,
 }
 
@@ -17,4 +22,5 @@ impl WallJumpManager {
             has_entered_window: false,
         }
     }
+
 }
