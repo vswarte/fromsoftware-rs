cb925b96454cd771c618e5e196522a0f5e370669
Vincent Swarte <vincent@vinsert.nl>
2026-02-09 00:23:31 +0100

    Change debug display to show state instead of ladder state

--- tools/debug-eldenring/src/display/chr.rs
+++ tools/debug-eldenring/src/display/chr.rs
@@ -657,7 +657,7 @@ impl DebugDisplay for ChrInsModuleContainer {
 impl DebugDisplay for CSChrLadderModule {
     fn render_debug(&self, ui: &Ui) {
         ui.text(format!("Ladder handle: {:?}", self.ladder_handle));
-        ui.text(format!("Ladder state: {:?}", self.ladder_state));
+        ui.text(format!("State: {:?}", self.state));
         ui.text(format!("Top: {:?}", self.top));
         ui.text(format!("Bottom: {:?}", self.bottom));
     }
