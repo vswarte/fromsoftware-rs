cb8c6235f34e17bdeb5be37a7b36da1c1f9d14f2
axd1x8a <sign@axd1x8a.me>
2026-03-05 09:19:01 +0300

    Update stl allocator trait to force non null return type

--- crates/shared/stl/src/allocator.rs
+++ crates/shared/stl/src/allocator.rs
@@ -1,20 +1,36 @@
-use std::ffi::c_void;
+use std::{
+    ffi::c_void,
+    mem::{MaybeUninit, align_of, size_of},
+    ptr::NonNull,
+};
 
 pub trait Allocator {
-    fn allocate_raw(&mut self, size: usize, align: usize) -> *mut c_void;
-
-    fn deallocate_raw(&mut self, ptr: *mut c_void) -> *mut c_void;
+    fn allocate_raw(&mut self, size: usize, align: usize) -> NonNull<c_void>;
+    fn deallocate_raw(&mut self, ptr: *mut c_void);
 }
 
 pub trait AllocatorExt {
-    fn allocate<T>(&mut self) -> *mut T;
+    fn allocate<T>(&mut self) -> NonNull<MaybeUninit<T>>;
+    fn allocate_n<T>(&mut self, count: usize) -> NonNull<[MaybeUninit<T>]>;
 }
 
-impl<A> AllocatorExt for A
-where
-    A: Allocator,
-{
-    fn allocate<T: Sized>(&mut self) -> *mut T {
-        self.allocate_raw(size_of::<T>(), align_of::<T>()) as _
+impl<A: Allocator> AllocatorExt for A {
+    fn allocate<T>(&mut self) -> NonNull<MaybeUninit<T>> {
+        self.allocate_raw(size_of::<T>(), align_of::<T>())
+            .cast::<MaybeUninit<T>>()
+    }
+
+    fn allocate_n<T>(&mut self, count: usize) -> NonNull<[MaybeUninit<T>]> {
+        let size = size_of::<T>()
+            .checked_mul(count)
+            .expect("allocation size overflow");
+
+        let ptr = self
+            .allocate_raw(size, align_of::<T>())
+            .cast::<MaybeUninit<T>>()
+            .as_ptr();
+
+        // Safety: ptr is non-null and we own `count` elements
+        unsafe { NonNull::new_unchecked(std::ptr::slice_from_raw_parts_mut(ptr, count)) }
     }
 }
