fb05e6984517a34ac46395548e1e4c4d493f6779
Natalie Weizenbaum <nex342@gmail.com>
2026-03-07 12:33:42 -0800

    [SDT] Basic AppMenu documentation

---
+++ crates/sekiro/src/sprj/scaleform_value.rs
@@ -0,0 +1,22 @@
+use shared::UnknownPtr;
+
+// Source of name: RTTI
+pub struct SprjScaleformValue {
+    _vftable: usize,
+    _unk08: u64,
+    _unk10: u64,
+    _unk18: UnknownPtr,
+    _unk20: u32,
+    _unk28: u64,
+    _unk30: u64,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn proper_sizes() {
+        assert_eq!(0x38, size_of::<SprjScaleformValue>());
+    }
+}
--- tools/debug-sekiro/src/display.rs
+++ tools/debug-sekiro/src/display.rs
@@ -1,6 +1,7 @@
 pub(crate) mod chr;
 pub(crate) mod game;
 pub(crate) mod item;
+pub(crate) mod menu;
 pub(crate) mod param;
 
 debug::define_debug_display!();
