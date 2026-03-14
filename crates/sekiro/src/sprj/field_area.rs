8c05ec92c83939c108c84a1a1540ce7d7f5096d4
Natalie Weizenbaum <nex342@gmail.com>
2026-03-13 21:15:48 -0700

    Fix FieldArea

--- crates/sekiro/src/sprj/field_area.rs
+++ crates/sekiro/src/sprj/field_area.rs
@@ -7,22 +7,40 @@ use shared::*;
 #[repr(C)]
 pub struct FieldArea {
     _vftable: usize,
+    _unk08: usize,
 
     pub world_info_owner: OwnedPtr<WorldRes>,
 
     _world_info_owner_2: Option<NonNull<WorldRes>>, // Always the same as [world_res], apparently
 
-    _game_rend: u64,
-    _unk20: u32,
-    _chr_cam: u64,
-    _unk30: [u8; 0x30],
-    _hit_ins: u64,
-    _unk68: u64,
-    _field_backread: usize,
-    _unk78: [u8; 0x60],
-    _self: NonNull<FieldArea>,
-    _unke0: usize,
-    _unke8: [u8; 8],
+    _game_rend: UnknownPtr,
+    _current_world_block_info_index: u32,
+    _chr_cam: UnknownPtr,
+    _unk38: u32,
+    _unk40: u64,
+    _unk48: u64,
+    _unk50: u32,
+    _unk58: u64,
+    _unk60: u64,
+    _hit_ins: UnknownPtr,
+    _unk70: u64,
+    _backread: OwnedPtr<UnknownStruct<0x78>>,
+    _unk80: u64,
+    _unk88: u64,
+    _unk90: u64,
+    _unk98: u64,
+    _unka0: u32,
+    _unka8: UnknownStruct<0x18>, // tree
+    _unkc0: u16,
+
+    pub debug_measurement_display: bool,
+    pub debug_major_reset: bool,
+
+    _unkc4: [u8; 0xc],
+    _unkd0: u32,
+    _unkd4: u32,
+    _unkd8: u32,
+    _unkdc: u32,
 }
 
 impl FromStatic for FieldArea {
@@ -32,5 +50,15 @@ impl FromStatic for FieldArea {
 
     unsafe fn instance() -> InstanceResult<&'static mut Self> {
         unsafe { shared::load_static_indirect(rva::get().field_area_ptr) }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn proper_sizes() {
+        assert_eq!(0xe0, size_of::<FieldArea>());
     }
 }
