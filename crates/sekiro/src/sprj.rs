6e928e7070e497e8b29790c50b219f2c0730598b
Natalie Weizenbaum <nex342@gmail.com>
2026-03-21 09:51:12 -0700

    [SDT] Document MenuMan (#266)

--- crates/sekiro/src/sprj.rs
+++ crates/sekiro/src/sprj.rs
@@ -1,6 +1,7 @@
 mod game_data_man;
 mod item;
 mod item_id;
+mod menu_man;
 mod player_game_data;
 mod scaleform_value;
 mod solo_param_repository;
@@ -10,6 +11,7 @@ mod window;
 pub use game_data_man::*;
 pub use item::*;
 pub use item_id::*;
+pub use menu_man::*;
 pub use player_game_data::*;
 pub use scaleform_value::*;
 pub use solo_param_repository::*;
