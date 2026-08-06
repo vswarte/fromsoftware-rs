3c7c994ad6fd3e73d70a7d6eb80a6d78fe148b21
moitt22 <mohi.hans.apel@gmail.com>
2026-08-07 01:04:40 +0200

    Fix comment (again)

--- crates/eldenring/src/cs/menu_man.rs
+++ crates/eldenring/src/cs/menu_man.rs
@@ -156,7 +156,7 @@ pub struct LoadingScreenData {
     pub start_fake_loading_screen: bool,
     pub end_loading_screen: bool,
     unk12: [u8; 2],
-    ///index of the current step that MoveMapStep is currently executing
+    ///index of the step that MoveMapStep is currently executing
     pub move_map_step_index: i32,
     ///fraction where bar starts advancing
     pub start_percentage: f32,
